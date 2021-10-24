3fced0d4ab5d8f0577274b138452fe5e1cb28c44
knoxg <knoxg@yttrium>
2021-10-24 16:53:38 +1000

    DagElement for subgraphs

--- src/main/java/com/randomnoun/build/javaToGraphviz/DagElement.java
+++ src/main/java/com/randomnoun/build/javaToGraphviz/DagElement.java
@@ -2,11 +2,13 @@ package com.randomnoun.build.javaToGraphviz;
 
 import java.util.Map;
 
+import org.jsoup.nodes.Attribute;
 import org.jsoup.nodes.Element;
 
 import com.randomnoun.build.javaToGraphviz.JavaToGraphviz.Dag;
 import com.randomnoun.build.javaToGraphviz.JavaToGraphviz.DagEdge;
 import com.randomnoun.build.javaToGraphviz.JavaToGraphviz.DagNode;
+import com.randomnoun.build.javaToGraphviz.JavaToGraphviz.DagSubgraph;
 import com.randomnoun.common.Text;
 
 public class DagElement extends Element {
@@ -15,19 +17,21 @@ public class DagElement extends Element {
     Dag dag;
     DagNode dagNode;
     DagEdge dagEdge;
+    DagSubgraph dagSubgraph;
     
     public DagElement(DagNode dagNode, Map<String, String> attributes) {
         super("node");
         this.attr("id", dagNode.name);
         this.attr("label", dagNode.label);
         attributes.entrySet().stream().forEach(e -> { 
-            // this.attributes().add(e.getKey(), e.getValue());
-            // 'style' attribute is used in CSS to store actual styles. guess we don't need to do that, do we.
             this.attr(e.getKey(), e.getValue());
         });
-        
         if (!dagNode.classes.isEmpty()) {
             this.attr("class", Text.join(dagNode.classes, " "));
+        }
+        if (attributes.containsKey("style")) {
+            // keep a copy of this as it may change when we apply the CSS the first time round to get the gv-newSubgraph properties
+            this.attr("original-style", attributes.get("style"));
         }
         this.dagNode = dagNode;
     }
@@ -35,25 +39,48 @@ public class DagElement extends Element {
     public DagElement(String tagName, Dag dag, Map<String, String> attributes) {
         super(tagName);
         attributes.entrySet().stream().forEach(e -> { 
-            // this.attributes().add(e.getKey(), e.getValue());
-            // 'style' attribute is used in CSS to store actual styles. guess we don't need to do that, do we.
             this.attr(e.getKey(), e.getValue());
         }); 
+        if (attributes.containsKey("style")) {
+            // keep a copy of this as it may change when we apply the CSS the first time round to get the gv-newSubgraph properties
+            this.attr("original-style", attributes.get("style"));
+        }
         this.dag = dag;
     }
 
     public DagElement(DagEdge dagEdge, Map<String, String> attributes) {
         super("edge");
         attributes.entrySet().stream().forEach(e -> { 
-            // this.attributes().add(e.getKey(), e.getValue());
-            // 'style' attribute is used in CSS to store actual styles. guess we don't need to do that, do we.
             this.attr(e.getKey(), e.getValue());
         }); 
         if (!dagEdge.classes.isEmpty()) {
             this.attr("class", Text.join(dagEdge.classes, " "));
         }
-
+        if (attributes.containsKey("style")) {
+            // keep a copy of this as it may change when we apply the CSS the first time round to get the gv-newSubgraph properties
+            this.attr("original-style", attributes.get("style"));
+        }
         this.dagEdge = dagEdge;
+    }
+
+    // new subgraph elements
+    public DagElement(DagSubgraph dagSubgraph, Map<String, String> attributes) {
+        super("subgraph");
+        if (!dagSubgraph.classes.isEmpty()) {
+            this.attr("class", Text.join(dagSubgraph.classes, " "));
+        }
+
+        // attributes are null when subgraphs are defined by CSS rules
+        if (attributes != null) {
+            attributes.entrySet().stream().forEach(e -> { 
+                this.attr(e.getKey(), e.getValue());
+            }); 
+            if (attributes.containsKey("style")) {
+                // keep a copy of this as it may change when we apply the CSS the first time round to get the gv-newSubgraph properties
+                this.attr("original-style", attributes.get("style"));
+            }
+        }
+        this.dagSubgraph = dagSubgraph;
     }
     
     
