b39c8acd70f5ebb768ccf72d24ac7a50999566db
knoxg <knoxg@yttrium.mshome.net>
2021-10-29 09:15:42 +1000

    comment only

--- src/main/java/com/randomnoun/build/javaToGraphviz/comment/CommentText.java
+++ src/main/java/com/randomnoun/build/javaToGraphviz/comment/CommentText.java
@@ -2,7 +2,14 @@ package com.randomnoun.build.javaToGraphviz.comment;
 
 import org.eclipse.jdt.core.dom.Comment;
 
-/** A source-code comment */
+/** A source-code comment.
+ * 
+ * <p>The AST object for a comment is {@link org.eclipse.jdt.core.dom.Comment}, but those are a bit painful 
+ * to use as they don't contain the comment text, and they're not in the CompilationUnit AST graph.
+ * 
+ * <p>We convert these to CommentText objects (or, if the comment begins with "gv", a subclass of CommentText),
+ * and associate these with the AST node that the comment is commenting on.
+ */
 public class CommentText {
     public Comment comment;
     public int line;
