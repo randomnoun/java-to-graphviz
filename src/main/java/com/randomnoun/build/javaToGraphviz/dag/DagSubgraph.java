023d3e5c633ce0d9a70c8a097f5251a5d0c9feaf
knoxg <knoxg@yttrium>
2021-10-29 14:45:16 +1000

    each diagram is a subgraph

--- src/main/java/com/randomnoun/build/javaToGraphviz/dag/DagSubgraph.java
+++ src/main/java/com/randomnoun/build/javaToGraphviz/dag/DagSubgraph.java
@@ -10,11 +10,11 @@ import java.util.Map.Entry;
 
 import com.randomnoun.common.Text;
 
-// in this data model, a subgraph is just used to group nodes together
-// (those nodes must already exist at the Dag level)
+// a DagSubgraph is a graphviz graph ( if container == null)
+// or a graphviz subgraph 
 public class DagSubgraph implements SubgraphHolder {
     public Dag dag; // top-level dag
-    public SubgraphHolder container;
+    public SubgraphHolder container; // if null, this is a root graph
     
     public String name;  // graphviz name
     public String label; // graphviz label
@@ -41,31 +41,82 @@ public class DagSubgraph implements SubgraphHolder {
     }
 
     public String toGraphviz(int indent) {
+        boolean isRoot = (container == null);
         var s = "";
         for (int i=0; i<indent; i++) { s += " "; }
         String labelText = label == null ? null : Text.replaceString(label, "\"",  "\\\"");
-        
+
+        /*
         String a = "";
         for (Entry<String, String> e : gvStyles.entrySet()) {
             if (!e.getKey().startsWith("gv-")) {  
                 a += s + "  " + e.getKey() + " = " + e.getValue() + ";\n";
             }
         }
+        */
         
-        String result = s + "subgraph " + name + " {\n" +
-          (label == null ? "" : s + "  label=\"" + labelText + "\";\n") +
-          a;
-        result += s + " ";
-        for (DagNode dn : nodes) {
-            // only draw nodes if they have an edge
-            boolean hasEdge = false;
-            for (DagEdge e : dag.edges) {
-                if (e.n1 == dn || e.n2 == dn) { hasEdge = true; break; }
-            }
-            if (hasEdge) {
-                result += " " + dn.name + ";";
+        String a = "", na = "", ea = "";
+        for (Entry<String, String> e : gvStyles.entrySet()) {
+            if (!e.getKey().startsWith("gv-")) {  // gv-wordwrap
+                a += s + "  " + e.getKey() + " = " + e.getValue() + ";\n";
             }
         }
+        for (Entry<String, String> e : gvNodeStyles.entrySet()) {
+            if (!e.getKey().startsWith("gv-")) {  // gv-wordwrap
+                na += s + "    " + e.getKey() + " = " + e.getValue() + ";\n";
+            }
+        }
+        for (Entry<String, String> e : gvEdgeStyles.entrySet()) {
+            if (!e.getKey().startsWith("gv-")) {  // gv-wordwrap
+                ea += s + "    " + e.getKey() + " = " + e.getValue() + ";\n";
+            }
+        }
+        
+        // return "digraph G {\n" +
+          
+        
+        
+        String result = s +  (isRoot ? "digraph " : "subgraph " + name) + " {\n" +
+          (label == null ? "" : s + "  label=\"" + labelText + "\";\n") +
+          (na.equals("") ? "" : "  node [\n" + na + "  ]\n") +
+          (ea.equals("") ? "" : "  edge [\n" + ea + "  ]\n") +
+          a;
+        
+        // TODO: if it's a root node, include nodes & edges here
+        if (isRoot) {
+            for (DagNode node : dag.nodes) {
+                // only draw nodes if they have an edge
+                boolean hasEdge = false;
+                for (DagEdge e : dag.edges) {
+                    if (e.n1 == node || e.n2 == node) { hasEdge = true; break; }
+                }
+                // if (node != methodNode && hasEdge) {
+                if (hasEdge) {
+                    result += node.toGraphviz(false) + "\n"; // isDebugLabel
+                }
+                // }
+            }
+            for (DagEdge edge : dag.edges) {
+                // if (edge.n1 != methodNode) {
+                    result += edge.toGraphviz() + "\n";
+                // }
+            }            
+
+        } else {
+            
+            result += s + " ";
+            for (DagNode dn : nodes) {
+                // only draw nodes if they have an edge
+                boolean hasEdge = false;
+                for (DagEdge e : dag.edges) {
+                    if (e.n1 == dn || e.n2 == dn) { hasEdge = true; break; }
+                }
+                if (hasEdge) {
+                    result += " " + dn.name + ";";
+                }
+            }
+        }
+        
         result += "\n";
         for (DagSubgraph sg : subgraphs) {
             result += sg.toGraphviz(indent + 2);
