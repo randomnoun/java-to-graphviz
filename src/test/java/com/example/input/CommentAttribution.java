de203e10506b6032b374df90a976d7b63e49ec0b
knoxg <knoxg@yttrium.mshome.net>
2021-11-08 08:49:14 +1000

    comment attribution test

--- src/test/java/com/example/input/CommentAttribution.java
+++ src/test/java/com/example/input/CommentAttribution.java
@@ -10,9 +10,17 @@ public class CommentAttribution {
        }
     */
 
+    /* gv-style: {
+    node {
+        gv-idFormat: "s_${lineNumber}";
+        // gv-labelFormat: "${lineNumber}: ${nodeType} lkn: ${lastKeepNodeId}";
+        gv-labelFormat: "${lineNumber} (${nodeType})"; 
+    }
+    */
+
 
     // gv: comment just before the method
-    public void testIf() {
+    public void testAttribution() {
         // gv: start of method
         
         // gv: this comment has it's own node
@@ -27,7 +35,7 @@ public class CommentAttribution {
         System.out.println("s4");  System.out.println("s5");  // gv: this comment is for s4 OR IS IT
         System.out.println("s6");  System.out.println("s7");  // gv:<: this comment is for s7
         /* gv:>:this comment is for s8 */ System.out.println("s8");  System.out.println("snot8");
-        System.out.println("s9");   /* gv:<--:this comment is for s7 */  System.out.println("s10");
+        System.out.println("s9");   /* gv:<--:this comment is for s9 */  System.out.println("s10");
         System.out.println("s11");  /* gv:-->:this comment is for s12 */ System.out.println("s12");
         
         System.out.println("s5"); /* gv: this comment is for s5 */  System.out.println("s6"); // gv: this comment is for s6 OR IS IT
