6770622f6a84e7690cf5ba49be0475e393e2773c
knoxg <knoxg@yttrium.mshome.net>
2021-12-10 15:29:45 +1000

    remove recursion in removeNodes()

--- src/main/java/com/randomnoun/build/javaToGraphviz/astToDag/DagNodeFilter.java
+++ src/main/java/com/randomnoun/build/javaToGraphviz/astToDag/DagNodeFilter.java
@@ -2,6 +2,7 @@ package com.randomnoun.build.javaToGraphviz.astToDag;
 
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Set;
 
@@ -130,147 +131,150 @@ public class DagNodeFilter {
     
 	private void removeNodes(DagNode node, boolean mergeEdges) { // Set<DagNode> seenNodes
 	    
-        List<DagEdge> inEdges = node.inEdges;
-        List<DagEdge> outEdges = node.outEdges;
-        Set<DagNode> redoNodes = new HashSet<>();
-    
-        /*
-        if (seenNodes.contains(node)) {
-            logger.error("loop detected: seen node " + node.name);
-            return;
-        } 
-        seenNodes.add(node);
-        */
+        Set<DagNode> redoNodes = new LinkedHashSet<>();
+        redoNodes.add(node);
         
-        if (mergeEdges && inEdges.size() > 0) {
-            
-            // if multiple in edges share a lastKeepNode then remove everything back to that keepNode
-            // and merge the edges
-            for (int i = 0; i < inEdges.size() - 1; i++) {
-                for (int j = i + 1; j < inEdges.size(); j++) {
-                    DagEdge inEdge1 = inEdges.get(i);
-                    DagEdge inEdge2 = inEdges.get(j);
-                    DagNode inEdge1KeepNode = inEdge1.n1.lastKeepNode;
-                    DagNode inEdge2KeepNode = inEdge2.n1.lastKeepNode;
-                    if (inEdge1KeepNode == inEdge2KeepNode /*&& inEdge1KeepNode != null*/) {
-                        logger.info("on node " + node.name + ", merged edges back to " + inEdge1KeepNode.name + ", i=" + i + ", j=" + j);
-                        DagEdge newEdge = new DagEdge();
-                        newEdge.n1 = inEdge1KeepNode;
-                        newEdge.n2 = node;
-                        // newEdge.label = "something";
-                        // format the edge a bit ?
-                        
-                        pruneEdges(inEdge1, node, inEdge1KeepNode);  
-                        pruneEdges(inEdge2, node, inEdge2KeepNode); 
-                        if ((newEdge.n1 != newEdge.n2) && !dag.hasEdge(newEdge.n1, newEdge.n2)) {
-                            dag.addEdge(newEdge);
+        while (!redoNodes.isEmpty()) {
+            node = redoNodes.iterator().next();
+            redoNodes.remove(node);
+            if (!dag.nodes.contains(node)) { continue; } // already removed
+
+            List<DagEdge> inEdges = node.inEdges;
+            List<DagEdge> outEdges = node.outEdges;
+
+            if (mergeEdges && inEdges.size() > 0) {
+                
+                // if multiple in edges share a lastKeepNode then remove everything back to that keepNode
+                // and merge the edges
+                for (int i = 0; i < inEdges.size() - 1; i++) {
+                    for (int j = i + 1; j < inEdges.size(); j++) {
+                        DagEdge inEdge1 = inEdges.get(i);
+                        DagEdge inEdge2 = inEdges.get(j);
+                        DagNode inEdge1KeepNode = inEdge1.n1.lastKeepNode;
+                        DagNode inEdge2KeepNode = inEdge2.n1.lastKeepNode;
+                        // if inEdge1KeepNode == null for both, then could remove diagram above this node ?
+                        if (inEdge1KeepNode == inEdge2KeepNode && inEdge1KeepNode != null) {
+                            logger.info("on node " + node.name + ", merged edges back to " + inEdge1KeepNode.name + ", i=" + i + ", j=" + j);
+                            DagEdge newEdge = new DagEdge();
+                            newEdge.n1 = inEdge1KeepNode;
+                            newEdge.n2 = node;
+                            // newEdge.label = "something";
+                            // format the edge a bit ?
+                            
+                            pruneEdges(inEdge1, node, inEdge1KeepNode);  
+                            pruneEdges(inEdge2, node, inEdge2KeepNode); 
+                            if ((newEdge.n1 != newEdge.n2) && !dag.hasEdge(newEdge.n1, newEdge.n2)) {
+                                dag.addEdge(newEdge);
+                            }
+                            
+                            redoNodes.add(inEdge1KeepNode);
+                            
+                            // restart from the first inEdge of this node 
+                            i=0;
+                            j=1;
                         }
-                        redoNodes.add(inEdge1KeepNode);
-                        
-                        // restart from the first inEdge of this node 
-                        i=0;
-                        j=1;
                     }
                 }
+                
             }
+    
+            // if there's zero or one edges leading in and zero or one leading out
+            // and there's no comment on this node, remove it
+            // (and continue tracing from the next node )
+            // logger.info("what about " + node.name);
             
-        }
-
-        // if there's zero or one edges leading in and zero or one leading out
-        // and there's no comment on this node, remove it
-        // (and continue tracing from the next node )
-        // logger.info("what about " + node.name);
-        
-        if (inEdges.size() == 0 && outEdges.size() == 1 && !node.keepNode) {
-
-            // if this was a rootNode, then the rootNode is now the next node 
+            if (inEdges.size() == 0 && outEdges.size() == 1 && !node.keepNode) {
+    
+                // if this was a rootNode, then the rootNode is now the next node 
+                
+                DagEdge outEdge = outEdges.get(0);
+                DagNode nextNode = outEdge.n2;
+                dag.removeEdge(outEdge);
+                
+                logger.info("removed start node " + node.type + ", " + node.lineNumber + ", " + node.name);
+                dag.nodes.remove(node);
+                redoNodes.add(nextNode);
+    
+            } else if (inEdges.size() == 1 && outEdges.size() == 0 && !node.keepNode) {
+    
+                DagEdge inEdge = inEdges.get(0);
+                dag.removeEdge(inEdge);
+                
+                logger.info("removed terminal node " + node.type + ", " + node.lineNumber + ", " + node.name);
+                dag.nodes.remove(node);
+    
+            } else if (inEdges.size() >= 1 && outEdges.size() == 1 && !node.keepNode) {
+                
+                // DagEdge inEdge = inEdges.get(0);
+                DagEdge outEdge = outEdges.get(0);
+    
+                for (DagEdge inEdge : new ArrayList<>(inEdges)) {
+                    if (inEdge.n1 == null) { continue; } // already removed
+                    if (inEdge.n2 == null) { continue; } // already removed
+                    if (inEdge.n1 == inEdge.n2) { continue; } // hmm
+                    DagEdge newEdge = new DagEdge();
+                    newEdge.n1 = inEdge.n1;
+                    newEdge.n2 = outEdge.n2;
+                    newEdge.label = inEdge.label;
+                    newEdge.back = inEdge.back || outEdge.back;
+                    // if (newEdge.back) { newEdge.classes.add("back"); }
+                    newEdge.classes.addAll(inEdge.classes);
+                    newEdge.classes.addAll(outEdge.classes);
+                    
+                    // if either edge was colored (break edges), the merged edge is as well
+                    // String color = inEdge.gvAttributes.get("color") == null ? outEdge.gvAttributes.get("color") : inEdge.gvAttributes.get("color");
+                    // if (color != null) { newEdge.gvAttributes.put("color", color); }
+                    
+                    newEdge.gvAttributes.putAll(inEdge.gvAttributes);
+                    newEdge.gvAttributes.putAll(outEdge.gvAttributes);
+                
+                    if (!dag.hasEdge(newEdge.n1, newEdge.n2)) {
+                        dag.addEdge(newEdge);
+                    } else {
+                        // merge classes, attributes, back flag into the existing edge ?
+                    }
+                    dag.removeEdge(inEdge);
+                }
+                
+                DagNode nextNode = outEdge.n2;
+                dag.removeEdge(outEdge);
+                
+                logger.info("removed node " + node.type + ", " + node.lineNumber + ", " + node.name);
+                dag.nodes.remove(node);
+                // start from n1 again as removing this node may make it possible to remove the parent node
+                redoNodes.add(nextNode);
             
-            DagEdge outEdge = outEdges.get(0);
-            DagNode nextNode = outEdge.n2;
-            dag.removeEdge(outEdge);
-            
-            logger.info("removed start node " + node.type + ", " + node.lineNumber + ", " + node.name);
-            dag.nodes.remove(node);
-            removeNodes(nextNode, mergeEdges); // seenNodes
-
-        } else if (inEdges.size() == 1 && outEdges.size() == 0 && !node.keepNode) {
-
-            DagEdge inEdge = inEdges.get(0);
-            dag.removeEdge(inEdge);
-            
-            logger.info("removed terminal node " + node.type + ", " + node.lineNumber + ", " + node.name);
-            dag.nodes.remove(node);
-
-        } else if (inEdges.size() == 1 && outEdges.size() == 1 && !node.keepNode) {
-            
-            DagEdge inEdge = inEdges.get(0);
-            DagEdge outEdge = outEdges.get(0);
-
-            DagEdge newEdge = new DagEdge();
-            newEdge.n1 = inEdge.n1;
-            newEdge.n2 = outEdge.n2;
-            newEdge.label = inEdge.label;
-            newEdge.back = inEdge.back || outEdge.back;
-            // if (newEdge.back) { newEdge.classes.add("back"); }
-            newEdge.classes.addAll(inEdge.classes);
-            newEdge.classes.addAll(outEdge.classes);
-            
-            // if either edge was colored (break edges), the merged edge is as well
-            // String color = inEdge.gvAttributes.get("color") == null ? outEdge.gvAttributes.get("color") : inEdge.gvAttributes.get("color");
-            // if (color != null) { newEdge.gvAttributes.put("color", color); }
-            
-            newEdge.gvAttributes.putAll(inEdge.gvAttributes);
-            newEdge.gvAttributes.putAll(outEdge.gvAttributes);
-            
-
-            // TODO: combine styles, probably
-            
-            dag.removeEdge(inEdge);
-            dag.removeEdge(outEdge);
-            if (!dag.hasEdge(newEdge.n1, newEdge.n2)) {
-                dag.addEdge(newEdge);
-            }
-            
-            logger.info("removed node " + node.type + ", " + node.lineNumber + ", " + node.name);
-            dag.nodes.remove(node);
-            // start from n1 again as removing this node may make it possible to remove the parent node
-            removeNodes(newEdge.n1, mergeEdges); // seenNodes
-        
-        } else if (true) { // mergeEdges
-            
-            // trace down the outEdges
-            
-            // logger.warn(node.outEdges.size() + " outEdges from node " + node.name);
-            for (DagEdge e : new ArrayList<DagEdge>(node.outEdges)) {
-                // logger.info("outEdges from " + node.type + ":" + node.line);
-                if (dag.edges.contains(e)) {
-                    if (dag.nodes.contains(e.n2)) { 
-                        if (!e.back) {
-                            // don't follow back edges
-                            // logger.info("checking " + e.n1.type + ":" + e.n1.line + " -> " + e.n2.type + ":" + e.n2.line + ", " + e.n2.name);
-                            removeNodes(e.n2);
+            } else if (true) { // mergeEdges
+                
+                // trace down the outEdges
+                
+                // logger.warn(node.outEdges.size() + " outEdges from node " + node.name);
+                for (DagEdge e : new ArrayList<DagEdge>(node.outEdges)) {
+                    // logger.info("outEdges from " + node.type + ":" + node.line);
+                    if (dag.edges.contains(e)) {
+                        if (dag.nodes.contains(e.n2)) { 
+                            if (!e.back) {
+                                // don't follow back edges
+                                // logger.info("checking " + e.n1.type + ":" + e.n1.line + " -> " + e.n2.type + ":" + e.n2.line + ", " + e.n2.name);
+                                redoNodes.add(e.n2);
+                            }
+                        } else {
+                            logger.warn("subnode " + e.n2.name + " missing from " + node.name);
+                            dag.removeEdge(e);
                         }
                     } else {
-                        logger.warn("subnode " + e.n2.name + " missing from " + node.name);
-                        dag.removeEdge(e);
+                        // logger.warn("edge " + e + " missing from " + node.name);
                     }
-                } else {
-                    // logger.warn("edge " + e + " missing from " + node.name);
+                }
+                
+                // if there's no outEdges or inEdges, check the children.
+                if (node.inEdges.size() == 0 && node.outEdges.size() == 0) {
+                    for (DagNode n : node.children) {
+                        redoNodes.add(n);
+                    }
                 }
             }
             
-            // if there's no outEdges or inEdges, check the children.
-            if (node.inEdges.size() == 0 && node.outEdges.size() == 0) {
-                for (DagNode n : node.children) {
-                    removeNodes(n);
-                }
-            }
-        }
-        
-        // if we've merged edges, may need to shorten paths again from the keepNodes
-        for (DagNode redo : redoNodes) {
-            logger.info("redoing " + node.name);
-            removeNodes(redo, false); // false = shorten only
         }
 	    
 	}
